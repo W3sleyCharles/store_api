b90e31a979a66f632acc6e8088f7fad5c2542f95
W3sleyCharles <168904811+W3sleyCharles@users.noreply.github.com>
2024-07-15 22:53:12 -0300

    minhas alterações

--- store/controllers/product.py
+++ store/controllers/product.py
@@ -8,17 +8,20 @@ from store.usecases.product import ProductUsecase
 
 router = APIRouter(tags=["products"])
 
-
+# Endpoint para criar um novo produto
 @router.post(path="/", status_code=status.HTTP_201_CREATED)
-async def post(
-    body: ProductIn = Body(...), usecase: ProductUsecase = Depends()
+async def criar_produto(
+    body: ProductIn = Body(...), 
+    usecase: ProductUsecase = Depends()
 ) -> ProductOut:
     return await usecase.create(body=body)
 
 
+# Endpoint para buscar um produto pelo ID
 @router.get(path="/{id}", status_code=status.HTTP_200_OK)
-async def get(
-    id: UUID4 = Path(alias="id"), usecase: ProductUsecase = Depends()
+async def buscar_produto(
+    id: UUID4 = Path(alias="id"), 
+    usecase: ProductUsecase = Depends()
 ) -> ProductOut:
     try:
         return await usecase.get(id=id)
@@ -26,13 +29,17 @@ async def get(
         raise HTTPException(status_code=status.HTTP_404_NOT_FOUND, detail=exc.message)
 
 
+# Endpoint para listar todos os produtos
 @router.get(path="/", status_code=status.HTTP_200_OK)
-async def query(usecase: ProductUsecase = Depends()) -> List[ProductOut]:
+async def listar_produtos(
+    usecase: ProductUsecase = Depends()
+) -> List[ProductOut]:
     return await usecase.query()
 
 
+# Endpoint para atualizar um produto pelo ID
 @router.patch(path="/{id}", status_code=status.HTTP_200_OK)
-async def patch(
+async def atualizar_produto(
     id: UUID4 = Path(alias="id"),
     body: ProductUpdate = Body(...),
     usecase: ProductUsecase = Depends(),
@@ -40,9 +47,11 @@ async def patch(
     return await usecase.update(id=id, body=body)
 
 
+# Endpoint para deletar um produto pelo ID
 @router.delete(path="/{id}", status_code=status.HTTP_204_NO_CONTENT)
-async def delete(
-    id: UUID4 = Path(alias="id"), usecase: ProductUsecase = Depends()
+async def deletar_produto(
+    id: UUID4 = Path(alias="id"), 
+    usecase: ProductUsecase = Depends()
 ) -> None:
     try:
         await usecase.delete(id=id)
